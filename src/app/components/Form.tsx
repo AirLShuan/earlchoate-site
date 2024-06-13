56e2fdebbbc2a4ce82f7d22cde6852d5b00d2d93
Earl Choate <earlchoate@Collegebook.lan>
2024-06-13 01:08:04 -0700

    <Learning>(<Started Building Forms React>): Added the first 5 chapters however, due to a hectic day and falling asleep didn't finish it.

---
+++ src/app/components/Form.tsx
@@ -0,0 +1,48 @@
+import { FormEvent, useState } from "react";
+
+const Form = () => {
+  const [person, setPerson] = useState({
+    name: '',
+    age: '',
+  });
+  const handleSubmit =(event: FormEvent) => {
+    event.preventDefault();
+    console.log(person);
+  }
+
+  return (
+    <form onSubmit={(handleSubmit)}>
+      <div className="mb-3">
+        <label htmlFor="name" className="form-label">
+            Name
+        </label>
+        <input
+          onChange={(event) => 
+            setPerson({ ...person, name: event.target.value })
+          }
+          value = {person.name}
+          id="name" 
+          type="text" 
+          className="form-control" 
+        />
+      </div>
+      <div className="mb-3">
+        <label htmlFor="age" className="form">
+            Age
+        </label>
+        <input
+          value = {person.age}
+          onChange={(event) =>
+            setPerson({ ...person, age: parseInt(event.target.value)}) 
+          } 
+          id="age" 
+          type="number" 
+          className="form-control"
+        />
+      </div>
+      <button className="btn btn-primary" type="submit">Submit</button>
+    </form>
+  )
+}
+
+export default Form
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,18 +9,14 @@ import Like from "./components/Like";
 import NavBar from "./components/NavBar";
 import Cart from "./components/Cart";
 import ExpandableText from "./components/ExpandableText";
+import Form from "./components/Form";
 
 export default function Page() {
   
   
   return (
     <div>
-      <ExpandableText>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam nam molestiae quasi, voluptatem similique ex quaerat accusantium magnam dignissimos quis nihil saepe commodi aspernatur sit corporis eveniet consequatur repudiandae ducimus. Porro recusandae qui, nam impedit inventore reiciendis obcaecati aliquid nobis voluptates aperiam optio possimus at error. Odio aspernatur autem nemo sunt iure facere eius id ipsa totam, numquam, aut quo quaerat excepturi dicta aliquid pariatur! Labore eos quibusdam aut velit eum, tempora quam enim veritatis consequuntur accusamus recusandae nesciunt explicabo corrupti mollitia non illo officiis sit esse exercitationem perspiciatis praesentium voluptate quasi. Sequi voluptatum impedit nobis unde consectetur asperiores voluptatem?
-      </ExpandableText>
-      {
-       
-      }
+      <Form></Form>
     </div>
   );
 }
