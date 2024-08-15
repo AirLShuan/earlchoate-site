10906291789f093a4261f5e22f42b8cad135654d
Earl Choate <earlchoate@Collegebook.lan>
2024-08-15 11:02:08 -0700

    <Learning>Got further in the lesson, some of the code doesn't work right now. Trying to fix it. (The new user cannot be deleted).

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -47,15 +47,44 @@ export default function Page() {
       })
   }
 
+  const addUser = () => {
+    const originalUsers = [...users];
+    const newUser = {id: 0, name: 'Earl' };
+    setUsers([newUser, ...users]);
+
+    axios.post('https://jsonplaceholder.typicode.com/users', newUser)
+      .then(({ data: savedUser }) => setUsers([savedUser, ...users]))
+      .catch(err => {
+        setError(err.message);
+        setUsers(originalUsers);
+      });
+  }
+
+  const updateUser = (user: User) => {
+    const originalUsers = [...users]
+    const updatedUser = {...user, name: user.name + '!'};
+    setUsers(users.map(u => u.id === user.id ? updatedUser: u));
+
+    axios.patch('https://jsonplaceholder.typicode.com/users' + user.id, updatedUser)
+      .catch(err => {
+        setError(err.message);
+        setUsers(originalUsers);
+      })
+  }
+
   return (
     <>
       {error && <p className='text-danger'>{error}</p>}
       {isLoading && <div className="spinner-border"></div>}
+      <button className='btn btn-primary mb-3' onClick={addUser}>Add</button>
       <ul className='list-group'>
         {users.map(user =>(
           <li key={user.id} className='list-group-item d-flex justify-content-between'>
             {user.name}
-            <button className='btn btn-outline-danger' onClick={() => deleteUser(user)}>Delete</button>
+            <div>
+              <button className="btn btn-outline-secondary mx-1" onClick={() => updateUser(user)}>Update</button>
+              <button className='btn btn-outline-danger' onClick={() => deleteUser(user)}>Delete</button>
+            </div>
           </li>
         ))}
       </ul>
