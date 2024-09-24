656960c0aecb76f2693ed9fd539985fe6cde285a
Earl Choate <earlchoate@Collegebook.local>
2024-09-24 00:01:16 -0700

    <Learning>Finished completely with connecting to the backend.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,28 +3,10 @@
 import { useState, useEffect } from "react";
 import { CanceledError } from './services/api-client';
 import userService, { User } from "./services/user-service";
+import useUsers from "./hooks/useUsers";
 
 export default function Page() {
-  const [users, setUsers] = useState<User []>([]);
-  const [error, setError] = useState('');
-  const [isLoading, setLoading] = useState(false);
-
-  useEffect(() => {
-
-    setLoading(true);
-    const {request, cancel} = userService.getAll<User>();
-      request.then((res) => {
-        setUsers(res.data);
-        setLoading(false);
-      })
-      .catch((err) => {
-        if (err instanceof CanceledError) return;
-        setError(err.message);
-        setLoading(false);
-      });
-
-    return () => cancel();
-  }, [])
+  const { users, error, isLoading, setUsers, setError } = useUsers();
 
   const deleteUser = (user: User) => {
     const originalUsers = [...users];
